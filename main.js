06ab498a7f691fb5e84dd858c627d95775a154b9
Sophia <un89313@gmail.com>
2024-07-10 21:23:02 +0900

    change api

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const api_Key= `c9f80cc1bb6145e595b972698153921b`
 let news = [];
 const getLatesNews = async () =>{
-  const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+  const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`);
   const response = await fetch(url)
   const data = await response.json()
   news = data.articles;
