5ab6a9ebc99e3defc12e0c88bf42699dd5548301
Sophia <un89313@gmail.com>
2024-07-13 00:16:15 +0900

    update refactoring errorhandling

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ const getLatesNews = async () =>{
 }
 
 
+
 //엔터키로 검색 키워드 입력
 const keyword_enter = document.getElementById("search-input");
 keyword_enter.addEventListener("keydown", function (event) {
@@ -28,39 +29,52 @@ keyword_enter.addEventListener("keydown", function (event) {
     getNewsKeyword();}
   })
 
+let url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?`);
+
+const getNews = async()=> {
+  try{
+    const response = await fetch(url)
+    const data = await response.json()
+    if(response.status === 200){
+      newsList = data.articles;
+      console.log(newsList)
+      render();
+    }else{
+      throw new Error(data.message)
+    }
+
+  }catch(error){
+    errorRender(error.massage)
+  }
+
+}
+
 const getNewsKeyword = async () => {
   const keyword = document.getElementById("search-input").value;
-
   if(keyword.length===0 || keyword==="" || keyword === " "){
     alert("검색 키워드를 입력해주세요!"); 
     return;
   } // 검색 키워드를 입력하지 않으면 alert가 뜸
-  const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&q=${keyword}`);
-  // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&apiKey=${api_Key}`);
-  const response = await fetch(url)
-  const data = await response.json()
-  newsList = data.articles;
-  render();
+  url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&q=${keyword}`);
+  // url = new URL(`https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&apiKey=${api_Key}`);
+  await getNews();
 
   keyword_enter.value = ""; // 검색 키워드 엔터치고 나면 입력창 비워둠
-
-  console.log("keyword", newsList )
 }
 
 const getnewsByCategory = async (event) => {
   const category = event.target.textContent.toLowerCase();
-  console.log(`category+ ${category}`);
-  const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&category=${category}`);
-  // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${api_Key}`);
-  const response = await fetch(url)
-  const data = await response.json()
-  newsList = data.articles;
-  render();
-  console.log("category", newsList )
+  url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&category=${category}`);
+  // url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${api_Key}`);
+  await getNews();
 
 }
 
-
+// const catchError = ()=> {
+//   try{
+//     newsList.map
+//   }
+// }
 const render=()=>{
   // 만약 찾는 검색어가 없으면 검색결과가 없다는 이미지가 나옴
   if(newsList.length===0){
@@ -91,16 +105,15 @@ const render=()=>{
         }
 
   //내용없음 표시 or 200장 이상일때 ... 으로 줄이는 부분은 조건부 연산자(물음표 연산자) 사용
-
+// title, description 누르면 뉴스 링크로 이동함
   
 }
 
-// const connectLink = () => {
-//   const description = document.getElementById("title");
+const errorRender = (errorMassage)=> {
+  const errorHTML =  `<div class="alert alert-danger" role="alert">  ${errorMassage} </div>`
+  document.getElementById("news-borad").innerHTML = errorHTML;
+}
 
-//   description.addEventListener("click", function (event) {
-
-// }
 
 const openNav = () => {
   document.getElementById("mySidenav").style.width = "250px";
